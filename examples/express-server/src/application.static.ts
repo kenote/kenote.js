4e3c7f458e29b96272b54adf76aef4789a1c509f
thondery <thondery@163.com>
2021-02-05 23:29:27 +0800

    remove express-server & koa-server

--- examples/express-server/src/application.static.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@kenote/core'
-import path from 'path'
-
-@Module({
-  statics: {
-    '/': path.resolve(process.cwd(), 'public')
-  }
-})
-export default class StaticModule {}
